874a1f26be3d7837d74706c5607e19fa4f091df0
i447092 <i447092@fhict.nl>
2021-01-14 06:23:55 +0100

    updated + created basic components
    updated header/footer
    
    created shoppingcart and search omponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './modules/user/header/components/header/header.component';
 import { FooterComponent } from './modules/user/footer/components/footer/footer.component';
+import { CartComponent } from './modules/user/shoppingcart/components/cart/cart.component';
+import { SearchbarComponent } from './modules/user/search/components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CartComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
